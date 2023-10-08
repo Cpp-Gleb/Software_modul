69a05d6b47712dadb84d397b40447b495aadf919
Gleb <115859857+Cpp-Gleb@users.noreply.github.com>
2023-10-08 16:01:23 +0300

    Create Notes.cpp

---
+++ Notes.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include<ctime>
+#include<cstdlib>
+
+class line {
+private:
+    std::string str;
+public:
+    void SetStr(int s) {
+        str = s;
+    }
+    int GetTime(){
+        //monitor clock, retern SS, HH, DD;
+    }
+};
+
+class block : public line{
+private:
+    //array of str
+public:
+    //get all notes per day
+    //get all blocks per month
+    //del last block per month
+};
+
+int main()
+{
+    
+}
+
